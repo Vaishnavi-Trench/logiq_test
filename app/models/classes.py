75a1c5aefd88dcc529cf2a15c5efa174eb6e3ba3
mytrench-admin <michael@trenchsecurity.ai>
2025-05-27 20:02:59 +0530

    additional classes

--- app/models/classes.py
+++ app/models/classes.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Literal, Union, Dict, Any, TypedDict, List, Optional
 
 
+############## sense related classes
 class AlertNameResponse(BaseModel):
     alert_name: str  # Define the expected structured output as a string
 
@@ -60,3 +61,63 @@ class VerdictExplanation(BaseModel):
     verdict_explanation: str = Field(
         description="Final conclusive explanation which justifies the given score. Enclose important keywords/data points within **Word** so that I can use it to highlight on UI."
     )
+
+
+########### genix related classes
+class TriageQuestion(BaseModel):
+    triage_question: str
+    pass_condition: str
+    severity: str
+
+
+class TriageQuestions(BaseModel):
+    questions: List[TriageQuestion]
+    ttp: str
+
+
+class ValidatedTriageQuestion(BaseModel):
+    isValid: bool
+    revised_triage_question: str
+    pass_condition: str
+    severity: str
+    validation_feedback: str
+
+
+class EnrichedStep(BaseModel):
+    use_case: str
+    agent_cot: List[str]
+
+
+class StepValidationFeedback(BaseModel):
+    isValid: bool
+    question_id: str
+    validation_issues: str
+    validation_feedback: str
+
+
+class PlanStepInputParams(BaseModel):
+    input_param_name: str
+    type: str  # metadata or tool_call
+    step_id: Optional[int] = (
+        None  # Optional, only if type is tool_call. Add default None.
+    )
+    field_name: str  # Field name to be extracted from the metadata or output of the tool_call step
+
+
+class PlanExecutionStep(BaseModel):
+    step_sequence_id: int
+    task: str
+    tool_name: str
+    input_params: List[PlanStepInputParams]
+
+
+class ExecutionPlan(BaseModel):
+    total_steps: int
+    steps: List[PlanExecutionStep]
+
+
+class BuildPlanValidatinFeedback(BaseModel):
+    isValid: bool
+    question_id: str
+    validation_issues: Optional[str] = None
+    validation_feedback: Optional[str] = None
