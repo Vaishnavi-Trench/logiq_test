40bc2446e8c5c2f4f97126fa3692b925e8001a5c
sgr-trench <gururags@trenchsecurity.ai>
2025-07-08 13:50:05 +0530

    input name change

--- app/services/intelligence/virustotal/utils.py
+++ app/services/intelligence/virustotal/utils.py
@@ -64,9 +64,9 @@ def fetch_ip_report(intcid, ip):
     Logger.info(f"IP report fetched for: {ip}, report: {report}")
     return report
 
-def fetch_hash_report(intcid, file_hash):
+def fetch_hash_report(intcid, hash):
     """Fetches file hash report from VirusTotal."""
-    Logger.info(f"Fetching file hash report for: {file_hash}, intcid: {intcid}")
+    Logger.info(f"Fetching file hash report for: {hash}, intcid: {intcid}")
 
     virustotal_data_query = {
         "intcid": intcid,
@@ -82,7 +82,7 @@ def fetch_hash_report(intcid, file_hash):
 
     vt_api_key = document.get("api_key")
 
-    url = f"https://www.virustotal.com/api/v3/files/{file_hash}"
+    url = f"https://www.virustotal.com/api/v3/files/{hash}"
     headers = {"accept": "application/json", "x-apikey": vt_api_key}
 
     response = requests.get(url, headers=headers, timeout=60)
@@ -98,14 +98,14 @@ def fetch_hash_report(intcid, file_hash):
     )
     
     report = {
-        "file_hash": file_hash,
+        "hash": hash,
         "status": "Valid file hash, report fetched",
         "malicious_count": malicious_count,
         "total_engines": total_engines,
         "reputation_score": reputation_score,
     }
     
-    Logger.info(f"File hash report fetched for: {file_hash}, report: {report}")
+    Logger.info(f"File hash report fetched for: {hash}, report: {report}")
     return report
 
 def fetch_url_report(intcid, url):
@@ -125,11 +125,20 @@ def fetch_url_report(intcid, url):
     )
 
     vt_api_key = document.get("api_key")
+    
+    data = {
+        "url": url
+        }
+    headers = {
+        "x-apikey": vt_api_key
+    }
+    url = "https://www.virustotal.com/api/v3/urls/"
+    response = requests.post(url, headers=headers, json=data, timeout=60)
 
-    url = f"https://www.virustotal.com/api/v3/urls/{url}"
-    headers = {"accept": "application/json", "x-apikey": vt_api_key}
+    # url = f"https://www.virustotal.com/api/v3/urls/{url}"
+    # headers = {"accept": "application/json", "x-apikey": vt_api_key}
 
-    response = requests.get(url, headers=headers, timeout=60)
+    # response = requests.get(url, headers=headers, timeout=60)
     response_data = response.json()
     attributes = response_data.get("data", {}).get("attributes", {})
     last_analysis_results = attributes.get("last_analysis_results", {})
